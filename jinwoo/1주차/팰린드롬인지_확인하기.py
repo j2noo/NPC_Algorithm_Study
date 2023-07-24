8985f36ff5b53c9c11ae2e80148ec56b38a6439e
j2noo <wlsdndml213@naver.com>
2023-07-24 10:52:03 +0900

    [1주차] 팰린드롬인지 확인하기.py

--- jinwoo/1주차/팰린드롬인지_확인하기.py
+++ jinwoo/1주차/팰린드롬인지_확인하기.py
@@ -1,2 +1,9 @@
-print("readmetest")
-# jinwoo 브랜치에서 수정합니다
+str = input()
+newStr = ""
+for idx in range(len(str) - 1, -1, -1):
+    newStr += str[idx]
+    
+if newStr == str:
+    print(1)
+else:
+    print(0)
