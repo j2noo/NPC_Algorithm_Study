a2264ad44ad56fd3f1e2eeeed3af3116b172dfbc
seunghye <129871487+bsh8798@users.noreply.github.com>
2023-08-10 18:56:16 +0900

    [3주차] 아무래도이문제는A번난이도인것같다

---
+++ seunghye/3주차/아무래도이문제는A번난이도인것같다.c
@@ -0,0 +1,15 @@
+#pragma warning (disable : 4996)
+#include <stdio.h>
+
+int main() {
+	int t, a, b;
+
+	scanf("%d", &t);
+
+	for (int i = 0; i < t; i++) {
+		scanf("%d %d", &a, &b);
+		printf("yes\n");
+	}
+
+	return 0;
+}
