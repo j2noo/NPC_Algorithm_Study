475cfd1192ac669d2a7ccf7252a4ec2d1d819d71
j2noo <wlsdndml213@naver.com>
2023-08-16 22:33:05 +0900

    [4주차] ㄷㄷㄷㅈ

--- jinwoo/4주차/ㄷㄷㄷㅈ.py
+++ jinwoo/4주차/ㄷㄷㄷㅈ.py
@@ -1,74 +1,38 @@
 import math
 import itertools
+import sys
 
-N = int(input())
+sys.setrecursionlimit(10**6)
+
+N = int(sys.stdin.readline())
 
 incidence_li = list([] for _ in range(N + 1))
+parent_li = [-1] * (N + 1)
+child_cnt = [0] * (N + 1)
+edge_li = []
 
 cntD, cntG = 0, 0
 
 for _ in range(N - 1):
-    u, v = map(int, input().split())
-    # v가 부모
-    if u > v:
-        incidence_li[v].append(u)
-    else:
-        incidence_li[u].append(v)
+    u, v = map(int, sys.stdin.readline().split())
+    incidence_li[v].append(u)
+    incidence_li[u].append(v)
+    edge_li.append([u, v])
 
 
-# for i in range(N + 1):
-#     print(incidence_li[i])
+def solveG(n):
+    cnt_children = len(incidence_li[n])
+    return math.comb(cnt_children, 3)
 
 
-# n번노드에서 ㄷ,ㅈ의 개수 1
-def solve1(n):
-    cnt = 0
-    for node_depth_2 in incidence_li[n]:
-        for node_depth_3 in incidence_li[node_depth_2]:
-            for node_depth_4 in incidence_li[node_depth_3]:
-                cnt += 1
-    return cnt
-
-
-# n번노드에서 ㄷ,ㅈ의 개수 2
-def solve2(n):
-    cnt = 0
-    for node_depth_2 in incidence_li[n]:
-        cnt_depth2 = len(incidence_li[node_depth_2])
-        cnt += math.comb(cnt_depth2, 2)
-    return cnt
-
-
-# n번노드에서 ㄷ,ㅈ의 개수 3
-def solve3(n):
-    cnt = 0
-    select_two = itertools.combinations(incidence_li[n], 2)
-    for li in select_two:
-        # print(list(li))
-        cnt += len(incidence_li[list(li)[0]])
-        cnt += len(incidence_li[list(li)[1]])
-    return cnt
-
-
-# n번노드에서 ㄷ,ㅈ의 개수 4
-def solve4(n):
-    cnt = 0
-    cnt_depth1 = len(incidence_li[n])
-    cnt += math.comb(cnt_depth1, 3)
-    return cnt
-
-
+# ㅈ
 for i in range(1, N + 1):
-    # print("solve1 : ", solve1(i))
-    # print("solve2 : ", solve2(i))
-    # print("solve3 : ", solve3(i))
-    # print("solve4 : ", solve4(i))
-    cntD += solve1(i)
-    cntG += solve2(i)
+    cntG += solveG(i)
 
-    cntD += solve3(i)
-
-    cntG += solve4(i)
+# ㄷ
+for edge in edge_li:
+    u, v = edge
+    cntD += (len(incidence_li[u]) - 1) * (len(incidence_li[v]) - 1)
 
 # print(f"ㄷ : {cntD} ㅈ : {cntG}")
 if cntD > cntG * 3:
